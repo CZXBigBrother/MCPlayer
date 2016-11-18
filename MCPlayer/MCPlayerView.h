046316f78bd52ce5f0424351ec43792e439f37bd
marcoChen <chenxing200838741@163.com>
2016-11-18 11:48:02 +0800

    添加使用注释

--- MCPlayer/MCPlayerView.h
+++ MCPlayer/MCPlayerView.h
@@ -19,15 +19,34 @@
 
 @interface MCPlayerView : UIView
 
+/**
+ *  生成单例,比如在tableview上使用的时候要保障内存的使用,平时的界面无所谓
+ */
 + (instancetype)sharedPlayerView;
-// 是否允许横屏播放
-@property(nonatomic, assign) BOOL allowFullScreen;
+/**
+ *  参数模型
+ */
 @property(nonatomic, strong) MCPlayerModel  *playModel;
 @property(nonatomic, weak)id<MCPlayerDelagate> delegate;
+/**
+ *  是否允许横屏播放
+ */
+@property(nonatomic, assign) BOOL allowFullScreen;
+/**
+ *  在tableview上使用的时候需要传入承载播放器的cell
+ */
 @property(nonatomic, strong)UITableViewCell *cell;
-
+/**
+ *  播放
+ */
 - (void)MCPlayerPlay;
+/**
+ *  暂停
+ */
 - (void)MCPlayerPause;
+/**
+ *  重置
+ */
 - (void)MCPlayerReset;
 + (void)setupViewToTopLocation:(UIView *)selfView withPlayerView:(UIView *)targetView;
 
